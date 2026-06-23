246b9b8c3aa7ed5d417039668c65aada4ddeb3e5
MD. Waris ahmad <118051823+Mdwaris@users.noreply.github.com>
2026-06-23 06:21:26 +0000

    conversion of DataType

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,38 @@
+ let score = "33abs"
+let score = "33"
+let score = null = ans = 0 
+ let score = undefined =  ans =  NaN
+ let score = true   = ans = 1
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// NOTE 
+// "33"  => 33
+// "33abs" => NaN
+// true => 1; false => 0;
+
+
+let  isLoggedIn = "waris"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "waris " => true 
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
