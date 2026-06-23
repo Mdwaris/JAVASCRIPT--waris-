0fff4d49048e60b45d23b6a6d7517a5894a69cc3
MD. Waris ahmad <118051823+Mdwaris@users.noreply.github.com>
2026-06-23 05:37:32 +0000

    DataTaype

---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,28 @@
+// LECTURE = 5   DATA TYPE AND ECMA STANDARDS 
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)   this show erroe because we work in node js not browser
+// for alert use have diffrent syntax. 
+console.log(3+3) // code readability should be high
+console.log("waris")
+
+let neme = "waris" // string datatype
+let age = 24 // number value
+let isLoggedIn = false
+
+let state = null //= empty
+// premative datatype
+// number => 2 to power 53 
+// bigint =>  not use very much only use use for bignumber
+//string =>  always use in  ""
+//boolean => true/ false
+//null => standalone value  null = empty
+// undefined =>  
+//symbol => unique
+
+//object 
+console.log(typeof age);
+console.log(typeof null); //object 
+console.log(typeof undefine); //// undefine
+
+
