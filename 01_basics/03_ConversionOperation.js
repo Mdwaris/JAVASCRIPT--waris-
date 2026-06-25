13d7d10d09a093360f29a9cf7f70b52c02284c05
MD. Waris ahmad <118051823+Mdwaris@users.noreply.github.com>
2026-06-25 05:02:24 +0000

    03_ConversionOperation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,4 +1,4 @@
- let score = "33abs"
+/* let score = "33abs"
 let score = "33"
 let score = null = ans = 0 
  let score = undefined =  ans =  NaN
@@ -36,3 +36,45 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+// ***************************************OPERATION**********************************************//
+let value = 3 
+let negvalue = -value
+// console.log(negvalue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+//console.log(2+2);
+
+let str1 = " hello"
+let str2 = " Waris"
+
+
+// let str3 = str1 +str2;
+// console.log(str3);
+// // explation in ECMA  topic = To Primitive
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 +"2");
+
+
+// console.log(( 3 + 4) * 5 % 3);
+
+// console.log(true);
+
+// console.log(+"");
+
+
+
+let num1,num2, num3
+num1 = num2 = num3 = 2+2 // wrong ways 
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+//study link on ECMA 
