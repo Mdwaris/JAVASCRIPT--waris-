9820aa2c21c60f4e0e2a5f866167c50515596557
MD. Waris ahmad <118051823+Mdwaris@users.noreply.github.com>
2026-06-25 05:20:47 +0000

    04_comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// this tye of cinversion put in confusion 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// comparision and equality check have diffrent.
+
+
+// === strict check  => check both value and datatype
+console.log("2" === 2);
+
+
+
+
