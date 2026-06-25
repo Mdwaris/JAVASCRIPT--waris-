c719c770825fcbb7c61d58738bb1a0b4f519f2ac
MD. Waris ahmad <118051823+Mdwaris@users.noreply.github.com>
2026-06-25 06:06:30 +0000

    Datatype-summary

---
+++ 01_basics/05_Datatypes-summary.js
@@ -0,0 +1,52 @@
+  //*****************************************SUMMARY***************************************************//
+  
+  // JAVASCRIPT IS DYNAMIC LANG.
+
+  const score = 100
+  const scoreValue = 100.3
+
+  const isLoggedIn = false
+  const outsideTemp = null
+  let userEmail;
+  const id = Symbol('124')
+
+  const anotherId = Symbol('124')
+  //console.log(id === anotherId);
+
+const bidNumber = 547382992000082653828n
+
+  /*
+    ##  PRIMITIVE => call by Values 
+            7 TYPE : String, Number, Boolean, Null,  Undefined, Symbol => use make value unique., 
+            BigInt
+
+
+        
+
+  ## Referance (non Primitive)
+                array, object, Function*/
+const heros = ["shaktiman","naagraaj","doga"] // => ARRAY
+ let myObj = {
+    name: "waris",
+
+    age: 24,
+} // OBJECT IS IN {}
+
+// FUNCTION
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+// console.log(typeof bigNumber)  => undefine // datatype la value pata karne ke liye use kar sakte hai => typeof ..... use kar ke
+
+// console.log(typeof scoreValue)  // number 
+
+
+//console.log(typeof myFunction)  // function
+
+// non primative ka return type ko object function hi bolte hai
+//function ka retun hai wo object function hota hai
+// symbole => symbole
+
+// STUDY LINK => https://262.ecma-international.org/5.1/#sec-11.4.3
